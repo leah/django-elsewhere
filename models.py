61a98f2ea909472b45b1f73186985fbb535d2b8e
leah.culver <leah.culver@b5d6d27f-ee38-0410-9a15-2b66d3990ea2>
2007-09-01 22:42:01 +0000

    changed field sizes again
    git-svn-id: https://django-psn.googlecode.com/svn/trunk@12 b5d6d27f-ee38-0410-9a15-2b66d3990ea2

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ from django_psn.util import NETWORK_IDS, MESSENGER_IDS
 class SocialNetworkProfile(models.Model):
     user = models.ForeignKey(User, primary_key=False, db_index=True, related_name=_('social_network_profiles'), raw_id_admin=True)
     network_id = models.CharField(maxlength=16, choices=NETWORK_IDS, db_index=True)
-    username = models.CharField(maxlength=32)
+    username = models.CharField(maxlength=64)
     date_added = models.DateTimeField(_('date added'), default=models.LazyDate(), auto_now_add="true")
     date_verified = models.DateTimeField(_('date verified'), default=models.LazyDate())
     is_verified = models.BooleanField(default=False)
@@ -19,7 +19,7 @@ class SocialNetworkProfile(models.Model):
 class InstantMessengerProfile(models.Model):
     user = models.ForeignKey(User, primary_key=False, db_index=True, related_name=_('instant_messenger_profiles'), raw_id_admin=True)
     messenger_id = models.CharField(maxlength=16, choices=MESSENGER_IDS, db_index=True)
-    username = models.CharField(maxlength=32)
+    username = models.CharField(maxlength=64)
     date_added = models.DateTimeField(_('date added'), default=models.LazyDate(), auto_now_add="true")
     date_verified = models.DateTimeField(_('date verified'), default=models.LazyDate())
     is_verified = models.BooleanField(default=False)
@@ -32,7 +32,7 @@ class InstantMessengerProfile(models.Model):
         
 class WebsiteProfile(models.Model):
     user = models.ForeignKey(User, primary_key=False, db_index=True, related_name=_('website_profiles'), raw_id_admin=True)        
-    name = models.CharField(maxlength=16)
+    name = models.CharField(maxlength=64)
     url = models.CharField(maxlength=250)
     date_added = models.DateTimeField(_('date added'), default=models.LazyDate(), auto_now_add="true")
     date_verified = models.DateTimeField(_('date verified'), default=models.LazyDate())
