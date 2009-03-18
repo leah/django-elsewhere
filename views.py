655b4412bfa6cff7e34d3911086ac3e21e4ce5e3
leahculver <leah.culver@gmail.com>
2009-03-18 15:37:16 -0700

    model forms instead of separate forms! cleaned up the example a bit

--- views.py
+++ views.py
@@ -11,45 +11,48 @@ from elsewhere.forms import *
 def example(request):
 
     if request.method == 'POST':
+
         new_data = request.POST.copy()
-        form_name = new_data['form_name']
+
         # Add forms
-        if form_name == 'sn_form':
-            sn_form = SocialNetworkSettingsForm(new_data)
-            if sn_form.is_valid():
-                profile = SocialNetworkProfile(user=request.user, network_id=sn_form.cleaned_data['network_id'], username=sn_form.cleaned_data['username'])
+        if new_data.get('sn-form') or new_data.get('im-form') or new_data.get('w-form'):
+
+            if new_data.get('sn-form'):
+                form = SocialNetworkForm(new_data)
+            elif new_data.get('im-form'):
+                form = InstantMessengerForm(new_data)
+            elif new_data.get('w-form'):
+                form = WebsiteForm(new_data)
+
+            if form.is_valid():
+                profile = form.save(commit=False)
+                profile.user = request.user
                 profile.save()
                 return HttpResponseRedirect(request.path)
-        elif form_name == 'im_form':
-            im_form = InstantMessengerSettingsForm(new_data)
-            if im_form.is_valid():
-                profile = InstantMessengerProfile(user=request.user, messenger_id=im_form.cleaned_data['messenger_id'], username=im_form.cleaned_data['username'])
-                profile.save()
-                return HttpResponseRedirect(request.path)
-        elif form_name == 'w_form':
-            w_form = WebsiteSettingsForm(new_data)
-            if w_form.is_valid():
-                profile = WebsiteProfile(user=request.user, name=w_form.cleaned_data['name'], url=w_form.cleaned_data['url'])
-                profile.save()
-                return HttpResponseRedirect(request.path)
+            else:
+                print form.errors
+
         # Delete forms
-        elif form_name == 'delete_sn_form':
-            request.user.social_network_profiles.get(id=request.POST['delete_id']).delete()
+        elif new_data.get('delete-sn-form') or new_data.get('delete-im-form') or new_data.get('delete-w-form'):
+            delete_id = request.POST['delete_id']
+
+            if new_data.get('delete-sn-form'):
+                request.user.social_network_profiles.get(id=delete_id).delete()
+            elif new_data.get('delete-im-form'):
+                request.user.instant_messenger_profiles.get(id=delete_id).delete()
+            elif new_data.get('delete-w-form'):
+                request.user.website_profiles.get(id=delete_id).delete()
+
             return HttpResponseRedirect(request.path)
-        elif form_name == 'delete_im_form':
-            request.user.instant_messenger_profiles.get(id=request.POST['delete_id']).delete()
-            return HttpResponseRedirect(request.path)
-        elif form_name == 'delete_w_form':
-            request.user.website_profiles.get(id=request.POST['delete_id']).delete()
-            return HttpResponseRedirect(request.path)
+
         # WTF?
         else:
             return HttpResponseServerError
-    else:
-        # Create blank forms
-        sn_form = SocialNetworkSettingsForm()
-        im_form = InstantMessengerSettingsForm()
-        w_form = WebsiteSettingsForm()
+
+    # Create blank forms
+    sn_form = SocialNetworkForm()
+    im_form = InstantMessengerForm()
+    w_form = WebsiteForm()
 
     return render_to_response('elsewhere/example.html', {
         'sn_form': sn_form, 'im_form': im_form, 'w_form': w_form,
