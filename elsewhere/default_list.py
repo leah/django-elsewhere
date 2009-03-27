df9f300ee804d16304883277142cbf25af2ad408
Chris Drackett <chris@chrisdrackett.com>
2009-03-26 21:54:19 -0500

    and now with the actual code back.. sorry about that.. *sigh*

---
+++ elsewhere/default_list.py
@@ -0,0 +1,440 @@
+default_social_networks = [
+    {
+        'name': '43 Things',
+        'url': 'http://www.43things.com/person/%s/',
+        'identifier': 'Username',
+        'icon': 'fortythreethings.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'Bebo',
+        'url': 'http://www.bebo.com/Profile.jsp?MemberId=%s',
+        'identifier': 'Member ID',
+        'icon': 'bebo.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'Blip.tv',
+        'url': 'http://%s.blip.tv/',
+        'identifier': 'Username',
+        'icon': 'blip.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'Catster',
+        'url': 'http://www.catster.com/cats/%s',
+        'identifier': 'Cat ID',
+        'icon': 'catster.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'Corkd',
+        'url': 'http://corkd.com/people/%s',
+        'identifier': 'Username',
+        'icon': 'corkd.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'Delicious',
+        'url': 'http://delicious.com/%s/',
+        'identifier': 'Username',
+        'icon': 'delicious.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'Digg',
+        'url': 'http://digg.com/users/%s/',
+        'identifier': 'Username',
+        'icon': 'digg.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'Django People',
+        'url': 'http://djangopeople.net/%s/',
+        'identifier': 'Username',
+        'icon': 'djangopeople.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'Dodgeball',
+        'url': 'http://www.dodgeball.com/user?uid=%s',
+        'identifier': 'User ID',
+        'icon': 'dodgeball.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'Dogster',
+        'url': 'http://www.dogster.com/dogs/%s',
+        'identifier': 'Dog ID',
+        'icon': 'dogster.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'Dopplr',
+        'url': 'http://www.dopplr.com/traveller/%s/',
+        'identifier': 'Username',
+        'icon': 'dopplr.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'Facebook',
+        'url': 'http://www.facebook.com/profile.php?id=%s',
+        'identifier': 'User ID',
+        'icon': 'facebook.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'Flickr',
+        'url': 'http://flickr.com/photos/%s/',
+        'identifier': 'Flickr Alias',
+        'icon': 'flickr.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'foursquare',
+        'url': 'http://playfoursquare.com/user?uid=%s',
+        'identifier': 'User ID',
+        'icon': 'foursquare.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'Gamer Card',
+        'url': 'http://live.xbox.com/en-US/profile/profile.aspx?pp=0&GamerTag=%s',
+        'identifier': 'Gamertag',
+        'icon': 'gamercard.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'GitHub',
+        'url': 'http://github.com/%s',
+        'identifier': 'Username',
+        'icon': 'github.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'GoodReads',
+        'url': 'http://www.goodreads.com/user/show/%s',
+        'identifier': 'User ID',
+        'icon': 'goodreads.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'Hi5',
+        'url': 'http://hi5.com/friend/profile/displayProfile.do?userid=%s',
+        'identifier': 'User ID',
+        'icon': 'hi5.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'Instructables',
+        'url': 'http://www.instructables.com/member/%s',
+        'identifier': 'Username',
+        'icon': 'instructables.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'Jaiku',
+        'url': 'http://%s.jaiku.com/',
+        'identifier': 'Username',
+        'icon': 'jaiku.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'Last.fm',
+        'url': 'http://www.last.fm/user/%s/',
+        'identifier': 'Username',
+        'icon': 'lastfm.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'LibraryThing',
+        'url': 'http://www.librarything.com/profile/%s',
+        'identifier': 'Username',
+        'icon': 'librarything.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'LinkedIn',
+        'url': 'http://www.linkedin.com/in/%s',
+        'identifier': 'Full Name (without spaces)',
+        'icon': 'linkedin.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'LiveJournal',
+        'url': 'http://%s.livejournal.com/',
+        'identifier': 'Username',
+        'icon': 'lj.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'Ma.gnolia',
+        'url': 'http://ma.gnolia.com/people/%s',
+        'identifier': 'Username',
+        'icon': 'magnolia.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'MetaFilter',
+        'url': 'http://www.metafilter.com/user/%s',
+        'identifier': 'User ID',
+        'icon': 'metafilter.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'MOG',
+        'url': 'http://mog.com/%s',
+        'identifier': 'Username',
+        'icon': 'mog.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'Multiply',
+        'url': 'http://%s.multiply.com/',
+        'identifier': 'Username',
+        'icon': 'multiply.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'MySpace',
+        'url': 'http://www.myspace.com/%s',
+        'identifier': 'Username',
+        'icon': 'myspace.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'Netvibes',
+        'url': 'http://www.netvibes.com/%s',
+        'identifier': 'Username',
+        'icon': 'netvibes.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'Newsvine',
+        'url': 'http://%s.newsvine.com/',
+        'identifier': 'Username',
+        'icon': 'newsvine.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'Ning',
+        'url': 'http://%s.ning.com/',
+        'identifier': 'Network Name',
+        'icon': 'ning.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'Orkut',
+        'url': 'http://www.orkut.com/Profile.aspx?uid=%s',
+        'identifier': 'User ID',
+        'icon': 'orkut.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'Pandora',
+        'url': 'http://pandora.com/people/%s',
+        'identifier': 'Username',
+        'icon': 'pandora.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'Plaxo',
+        'url': 'http://%s.myplaxo.com/',
+        'identifier': 'Public Profile ID',
+        'icon': 'plaxo.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'Pownce',
+        'url': 'http://pownce.com/%s/',
+        'identifier': 'Username',
+        'icon': 'pownce.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'Readernaut',
+        'url': 'http://readernaut.com/%s/',
+        'identifier': 'Username',
+        'icon': 'readernaut.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'RedBubble',
+        'url': 'http://www.redbubble.com/people/%s/',
+        'identifier': 'Username',
+        'icon': 'redbubble.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'Reddit',
+        'url': 'http://reddit.com/user/%s/',
+        'identifier': 'Username',
+        'icon': 'reddit.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'Seesmic',
+        'url': 'http://seesmic.com/%s/',
+        'identifier': 'Username',
+        'icon': 'seesmic.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'Shelfworthy',
+        'url': 'http://shelfworthy.com/%s/',
+        'identifier': 'Username',
+        'icon': 'shelfworthy.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'SonicLiving',
+        'url': 'http://www.sonicliving.com/user/%s/',
+        'identifier': 'User ID',
+        'icon': 'sonicliving.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'StumbleUpon',
+        'url': 'http://%s.stumbleupon.com/',
+        'identifier': 'Username',
+        'icon': 'stumbleupon.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'Tabblo',
+        'url': 'http://www.tabblo.com/studio/person/%s/',
+        'identifier': 'Username',
+        'icon': 'tabblo.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'TagWorld',
+        'url': 'http://www.tagworld.com/%s',
+        'identifier': 'Username',
+        'icon': '',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'Technorati',
+        'url': 'http://technorati.com/people/technorati/%s',
+        'identifier': 'Username',
+        'icon': 'technorati.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'Tribe',
+        'url': 'http://people.tribe.net/%s',
+        'identifier': 'Username',
+        'icon': 'tribe.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'Tumblr',
+        'url': 'http://%s.tumblr.com/',
+        'identifier': 'Username',
+        'icon': 'tumblr.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'Twitter',
+        'url': 'http://twitter.com/%s',
+        'identifier': 'Username',
+        'icon': 'twitter.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'Upcoming',
+        'url': 'http://upcoming.yahoo.com/user/%s',
+        'identifier': 'User ID',
+        'icon': 'upcoming.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'Ustream.TV',
+        'url': 'http://www.ustream.tv/%s',
+        'identifier': 'Username',
+        'icon': 'ustream.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'Virb',
+        'url': 'http://www.virb.com/%s',
+        'identifier': 'Username',
+        'icon': 'virb.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'Vox',
+        'url': 'http://%s.vox.com/',
+        'identifier': 'Username',
+        'icon': 'vox.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'Wakoopa',
+        'url': 'http://wakoopa.com/%s',
+        'identifier': 'Username',
+        'icon': 'wakoopa.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'YouTube',
+        'url': 'http://www.youtube.com/user/%s',
+        'identifier': 'Username',
+        'icon': 'youtube.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'Zooomr',
+        'url': 'http://www.zooomr.com/photos/%s',
+        'identifier': 'Username',
+        'icon': 'zooomr.png',
+        'network_type': 'sn'
+    },
+    {
+        'name': 'Zune',
+        'url': 'http://social.zune.net/member/%s',
+        'identifier': 'Username',
+        'icon': 'zune.png',
+        'network_type': 'sn'
+    },
+]
+
+default_im_networks = [
+    {
+        'name': 'AIM',
+        'url': 'aim:goim?screenname=%s',
+        'icon': 'aim.png',
+        'network_type': 'im'
+    },
+    {
+        'name': 'GTalk',
+        'url': 'gtalk:chat?jid=%s',
+        'icon': 'gtalk.png',
+        'network_type': 'im'
+    },
+    {
+        'name': 'ICQ',
+        'url': 'http://www.icq.com/people/about_me.php?to=%s',
+        'icon': 'icq.png',
+        'network_type': 'im'
+    },
+    {
+        'name': 'MSN',
+        'url': 'msnim:chat?contact=%s',
+        'icon': 'msn.png',
+        'network_type': 'im'
+    },
+    {
+        'name': 'Skype',
+        'url': 'skype:%s?chat',
+        'icon': 'skype.png',
+        'network_type': 'im'
+    },
+    {
+        'name': 'Y!',
+        'url': 'ymsgr:sendim?%s',
+        'icon': 'yahoo.png',
+        'network_type': 'im'
+    },
+]
---
+++ elsewhere/urls.py
@@ -0,0 +1,27 @@
+import settings
+import os
+
+from django.conf.urls.defaults import *
+from django.contrib import admin
+
+from functions import fill_db
+
+admin.autodiscover()
+
+urlpatterns = patterns('elsewhere.views',
+    (r'^$', 'example'),
+)
+
+if settings.DEBUG:
+
+    CUR_DIR = os.path.dirname(__file__)
+    IMG_PATH = 'img/'
+    IMG_DIR = os.path.join(CUR_DIR, "img")
+
+    urlpatterns += patterns('django.views',
+        url(r'^%s(?P<path>.*)' % IMG_PATH, 'static.serve', {'document_root': IMG_DIR}, name='elsewhere_img')
+    )
+
+# fill the database if it hasn't been filled already
+print "filling DB"
+fill_db()
